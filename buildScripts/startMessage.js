3b51ab5cd03292b0bffa70271b002c9bcf7886b9
kwegiel <ken.wegiel@pioneerinvestments.com>
2017-05-18 12:31:11 -0400

    added babel transpiling

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,7 @@
-var chalk = require('chalk');
+//ES5
+//var chalk = require('chalk');
+
+//ES6 - use babel to transpile
+import chalk from 'chalk';
 
 console.log(chalk.green('Starting app in dev mode...'));
