e673ad16b9423a730b022c34f1eda7337906db87
kwegiel <ken.wegiel@pioneerinvestments.com>
2017-05-22 16:48:56 -0400

    added http (using fetch), mock data (using json-schema-faker), and mock api (using json-server)

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -28,6 +28,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Ben","lastName":"Kenobi","email":"obiwan@jedicouncil.net"},
+    {"id": 2,"firstName":"Luke","lastName":"Skywalker","email":"luke@tosche.com"},
+    {"id": 3,"firstName":"Wedge","lastName":"Antilles","email":"wedge@hotmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
